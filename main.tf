7cc58681bbb2d473f2b54370aa05bc684e93291f
chakradhar933 <107048350+chakradhar933@users.noreply.github.com>
2022-07-28 09:38:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,10 +8,10 @@ terraform {
   provider "aws" {
   region = "us-east-1"
 }
-resource "aws_instance" "web" {
+
+resource "aws_instance" "edstem" {
   ami           = "ami-0cff7528ff583bf9a"
   instance_type = "t2.micro"
-
   tags = {
     Name = "HelloWorld"
   }
