f567c47a67cdac8751e4f46f77d50dc4b786bd34
ram <chakradhar.thatikonda933@gmail.com>
2022-07-28 09:10:32 +0530

    hi

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+  provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "web" {
+  ami           = "ami-0cff7528ff583bf9a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
